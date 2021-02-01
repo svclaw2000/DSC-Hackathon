12b67a75bd8862f6db5ab6e0747edee27fe97f5f
Hyesung <comet_0419@naver.com>
2021-02-02 00:28:21 +0900

    Add my activity layout

--- Android/Goorum/app/src/main/java/com/example/goorum/MyActivity.kt
+++ Android/Goorum/app/src/main/java/com/example/goorum/MyActivity.kt
@@ -1,11 +1,21 @@
 package com.example.goorum
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.goorum.databinding.ActivityMyBinding
 
 class MyActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_my)
+
+        binding.ivMyBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
