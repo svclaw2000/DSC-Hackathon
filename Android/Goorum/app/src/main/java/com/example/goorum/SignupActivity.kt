4863477d1640cf79f181ecc6b2eb9b484715673c
Hyesung <comet_0419@naver.com>
2021-02-04 00:45:16 +0900

    Modify signup layout

--- Android/Goorum/app/src/main/java/com/example/goorum/SignupActivity.kt
+++ Android/Goorum/app/src/main/java/com/example/goorum/SignupActivity.kt
@@ -4,16 +4,30 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Window
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.example.goorum.data.Signup
 import com.example.goorum.databinding.ActivitySignupBinding
+import java.util.regex.Pattern
 
 class SignupActivity : AppCompatActivity() {
     val TAG = "SignupActivity"
+    val EMAIL_ADDRESS_PATTERN = Pattern.compile(
+        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+"
+    )
+    val submit = Signup();
 
     lateinit var binding: ActivitySignupBinding
     lateinit var email: String
     lateinit var password: String
+    lateinit var nickname: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +39,20 @@ class SignupActivity : AppCompatActivity() {
         }
 
         binding.bRegister.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            intent.flags = (Intent.FLAG_ACTIVITY_NEW_TASK
-                    or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            email = binding.etEmail.editText?.text.toString()
+            password = binding.etCreatePw.editText?.text.toString()
+            nickname = binding.etNickname.editText?.text.toString()
+            Log.d(TAG, "email: $email / password: $password / nickname: $nickname")
 
-            onSubmit()
-
-            startActivity(intent)
+            if (email.equals("") || password.equals("") || nickname.equals("")) {
+                Toast.makeText(this, "모든 정보를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            } else if (!isValidEmail(email)) {
+                Toast.makeText(this, "유효하지 않은 이메일입니다.", Toast.LENGTH_SHORT).show()
+            } else if (!matchesThePassword()) {
+                Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                onSubmit()
+            }
         }
     }
 
@@ -42,10 +63,27 @@ class SignupActivity : AppCompatActivity() {
     }
 
     fun onSubmit() {
-        email = binding.etSignup.text.toString()
-        password = binding.etCreatePw.text.toString()
-        Log.d(TAG, "email: $email / password: $password")
+        Log.d(TAG, "회원가입 완료, email: $email / password: $password / nickname: $nickname")
 
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = (Intent.FLAG_ACTIVITY_NEW_TASK
+                or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        submit.email = email
+        submit.password = password
+        submit.nickname = nickname
         // TODO: server로 전달
+
+        startActivity(intent)
+    }
+
+    fun isValidEmail(str: String): Boolean{
+        return EMAIL_ADDRESS_PATTERN.matcher(str).matches()
+    }
+
+    fun matchesThePassword(): Boolean {
+        val confirm = binding.etConfirmPw.editText?.text.toString()
+        if (password == confirm) return true
+        return false
     }
 }
---
+++ Android/Goorum/app/src/main/java/com/example/goorum/data/Signup.kt
@@ -0,0 +1,20 @@
+package com.example.goorum.data
+
+import org.json.JSONObject
+
+
+class Signup() {
+    lateinit var email: String
+    lateinit var password: String
+    lateinit var nickname: String
+
+    fun compareEmail(): Boolean {
+       return true
+    }
+
+    fun register(): Boolean {
+        return true
+    }
+}
+
+
